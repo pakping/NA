fadc925a35b173aff682713f12255c6fea142b2a
pakping <pakping-up@hotmail.com>
2021-01-28 23:49:46 +0700

    first commit

---
+++ components/navbar.php
@@ -0,0 +1,8 @@
+<!-- Just an image -->
+<nav class="navbar navbar-light bg-light" style="background-color: #fff;">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.thenathailand.com/wp-content/uploads/2020/11/Nathailand-120x64-1.png" alt="" width="120" height="64">
+    </a>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>THE NA TH </title>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="./css/styme.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    include './components/navbar.php'
+    ?>
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                    <h1 class="display-4">THE NA COSMETIC</h1>
+                    <p class="lead"></p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+</div>
+            
+        </div>
+    </div>
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</body>
+
+
+</html>
