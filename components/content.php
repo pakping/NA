7fb6c0ca65cee8cc0650f7d5cdf64f9ee1cb3b8f
jee <guitar_104@hotmail.com>
2021-02-02 13:47:22 +0700

    while

--- components/content.php
+++ components/content.php
@@ -1,51 +1,25 @@
 <!-- Page Content -->
 <div class="container">
   <div class="row">
-
-    <!-- Team-->
-    <div class="col-xl-4 col-md-6 mb-4">
-      <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/by7IeC6Qa0k/500x350" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="card-title mb-0">Food supplement1</h5>
-          <br>
-          <button type="button" class="btn btn-outline-primary btn-auto btn-block">More</button>
+    <?php
+    $a = 0;
+    while ($a <= 2) {
+      $a += 1;
+    ?>
+      <!-- Team-->
+      <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-0 shadow">
+          <img src="https://source.unsplash.com/by7IeC6Qa0k/500x350" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title mb-0">Food supplement1</h5>
+            <br>
+            <button type="button" class="btn btn-outline-primary btn-auto btn-block">More</button>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Team -->
-    <div class="col-xl-4 col-md-6 mb-4">
-      <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/jUPOXXRNdcA/500x350" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="card-title mb-0">Food supplement2</h5>
-          <br>
-          <button type="button" class="btn btn-outline-primary btn-auto btn-block">More</button>
-        </div>
-      </div>
-    </div>
-    <!-- Team -->
-    <div class="col-xl-4 col-md-6 mb-4">
-      <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/VRB1LJoTZ6w/500x350" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="card-title mb-0">Food supplement3</h5>
-          <br>
-          <button type="button" class="btn btn-outline-primary btn-auto btn-block">More</button>
-        </div>
-      </div>
-    </div>
-    <!-- Team -->
-    <div class="col-xl-4 col-md-6 mb-4">
-      <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/HPZs4EXRFSU/500x350" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="card-title mb-0">Food supplement4</h5>
-          <br>
-          <button type="button" class="btn btn-outline-primary btn-auto btn-block">More</button>
-        </div>
-      </div>
-    </div>
+    <?php
+    }
+    ?>
 
 
   </div>
