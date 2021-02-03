14ceffe3df209379d28f92540f473396de417ecb
pakping <pakping-up@hotmail.com>
2021-02-03 21:54:23 +0700

    fix index

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,8 @@
 
-
-<footer  id="sticky-footer"  class="p-3 bg-light text-white-90" style="height: 100px;" >
-    <div class="container">
+<footer id="sticky-footer" class=" shadow-sm p-3 bg-light text-white-90 " style="height: 100px;">
+    <div class="container ">
         <div>
+
             <div class="float-right">
             <div>
                 <p>ติดต่อเรา</p>
@@ -25,21 +25,17 @@
                 </a>
             </div>
             
-            
-           
+    
         </div>
     </div>
 </footer>
 
 
-<footer  id="sticky-footer"  class="p-3 bg-light text-white-90" style="height: 100px;" >
-    <div class="container">
-        <div>
-            <!-- <p class="float-right"><a href="#">กลับไปหน้าบน</a></p> -->
-            <p>&copy; ห้ามนำข้อมูลไปเผยแพร่ก่อนได้รับอนุญาต &middot;</p>
-            </small>
-        </div>
-    </div>
+
+<footer class="footer mt-auto py-3 bg-light ">
+  <div class="container">
+    <span class="text-muted">&copy; ห้ามนำข้อมูลไปเผยแพร่ก่อนได้รับอนุญาต &middot;</span>
+  </div>
 </footer>
 
 
@@ -47,6 +43,3 @@
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-
-<!-- <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script> -->
--- index.php
+++ index.php
@@ -1,91 +1,170 @@
  <!-- Check Sessionpersist
 ===================================================================================================-->
-<?php
-include 'auth/Sessionpersist.php';
-?>
-<!--END  Check Sessionpersist
+ <?php
+  include 'auth/Sessionpersist.php';
+  ?>
+ <!--END  Check Sessionpersist
 ===================================================================================================-->
 
-<!doctype html>
-<html lang="en">
+ <!doctype html>
+ <html lang="en">
 
-<head>
-    <!-- Required meta tags
+ <head>
+   <!-- Required meta tags
 ===================================================================================================-->
-    <title>The Na Thailand</title>
-    <meta charset="utf-8">
-    <META NAME="keywords" CONTENT="The Na Thailand">
-    <META NAME="description" CONTENT="สินค้าดูแลสุขภาพและความงาม จากผู้เชี่ยวชาญ">
+   <title>The Na Thailand</title>
+   <meta charset="utf-8">
+   <META NAME="keywords" CONTENT="The Na Thailand">
+   <META NAME="description" CONTENT="สินค้าดูแลสุขภาพและความงาม จากผู้เชี่ยวชาญ">
 
-    <!-- END Required meta tags
+   <!-- END Required meta tags
 ===================================================================================================-->
-<?php
+   <?php
     include 'components/head/head.php'
     ?>
-</head>
 
-<body>
+   <!-- CSS
+===================================================================================================-->
+   <link rel="stylesheet" href="css/style.css">
+   <!-- CSS
+===================================================================================================-->
+   <style>
+     .carousel-item {
+       height: 65vh;
+       min-height: 300px;
+       background: no-repeat center center scroll;
+       -webkit-background-size: cover;
+       -moz-background-size: cover;
+       -o-background-size: cover;
+       background-size: cover;
+     }
+     body {
+    background: no-repeat center center fixed;
+    background-color: white;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    background-size: cover;
+    -o-background-size: cover;
 
-    <!-- Navbar
+}
+section {padding:2rem;}
+
+.bg-gray {background-color:#F0F8FF
+;}
+   </style>
+ </head>
+
+ <body>
+
+   <!-- Navbar
  ===================================================================================================-->
-    <?php
-    include 'components/navbar/navbar.php'
-    ?>
-    <!--END  Navbar
+   <header>
+     <?php
+      include 'components/navbar/navbar.php'
+      ?>
+   </header>
+   <!--END  Navbar
+   
 ===================================================================================================-->
 
-    <!-- Header photo 
+   <main>
+     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+       <ol class="carousel-indicators">
+         <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+         <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+         <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+       </ol>
+       <div class="carousel-inner">
+         <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+
+           <div class="carousel-caption d-none d-md-block">
+             <h5>First slide label</h5>
+             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+           </div>
+         </div>
+         <div class="carousel-item" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+
+           <div class="carousel-caption d-none d-md-block">
+             <h5>Second slide label</h5>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+           </div>
+         </div>
+         <div class="carousel-item" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+
+           <div class="carousel-caption d-none d-md-block">
+             <h5>Third slide label</h5>
+             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+           </div>
+         </div>
+       </div>
+       <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden"></span>
+       </a>
+       <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden"></span>
+       </a>
+     </div>
+
+
+    <!--Page Content
+===================================================================================================-->
+     <section class=" shadow-sm bg-gray text-center" >
+   <div class="container">
+     
+
+           <section class="py-5">
+             <div class="container">
+               <h3>-- ช่องทางการติดต่อ --</h3>
+               <p class="lead"> <a href="https://www.facebook.com/chititpat.Sripoot">faebook-pakping</a> <a href="https://www.facebook.com/jeerachon9/">facebook-G</a>
+                 <a href="https://www.facebook.com/profile.php?id=100002182427958">facebook-big</a>
+               </p>
+             </div>
+           </section>
+
+           <!-- end crad body -->
+       </div>
+       <!-- end container -->
+     </section>
+     <!--END Page Content
 ===================================================================================================-->
 
-    <?php
-    include 'components/header.php'
-    ?>
-    <!-- END Header photo
+
+
+     <!-- Pagae content-photo-library
 ===================================================================================================-->
 
-    <br>
-    <!-- Page Content
+     <?php
+      include 'components/content/content.php'
+      ?>
+     <!--END  Pagae content-photo-library
 ===================================================================================================-->
-    <div class="container">
+<section class=" shadow-sm bg-gray text-center" >
+   <div class="container">
+     
 
-        <div class="card border-0 shadow my-5">
+           <section class="py-5">
+             <div class="container">
+               <h3>-- ช่องทางการติดต่อ --</h3>
+               <p class="lead"> <a href="https://www.facebook.com/chititpat.Sripoot">faebook-pakping</a> <a href="https://www.facebook.com/jeerachon9/">facebook-G</a>
+                 <a href="https://www.facebook.com/profile.php?id=100002182427958">facebook-big</a>
+               </p>
+             </div>
+           </section>
 
-            <div class="card-body p-5">
-
-                <section class="py-5">
-                    <div class="container">
-                        <h5 class="display-4">-- ช่องทางการติดต่อ --</h5>
-                        <p class="lead"> <a href="https://www.facebook.com/chititpat.Sripoot">faebook-pakping</a> <a href="https://www.facebook.com/jeerachon9/">facebook-G</a>
-                            <a href="https://www.facebook.com/profile.php?id=100002182427958">facebook-big</a>
-                        </p>
-                    </div>
-                </section>
-
-                <!-- end crad body -->
-            </div>
-            <!-- end card shadow  -->
-        </div>
-        <!-- end container -->
-    </div>
-    <!--END Page Content
+           <!-- end crad body -->
+       </div>
+       <!-- end container -->
+     </section>
+     <!-- start footer 
 ===================================================================================================-->
-
-
-    <!-- Pagae content-photo-library
+     <?php
+      include 'components/footer.php'
+      ?>
+   </main>
+   <!--END start footer 
 ===================================================================================================-->
-<?php
-  include 'components/content/content.php'
-  ?>
-    <!--END  Pagae content-photo-library
-===================================================================================================-->
+ </body>
 
-    <!-- start footer 
-===================================================================================================-->
-<?php
-  include 'components/footer.php'
-  ?>
-    <!--END start footer 
-===================================================================================================-->
-</body>
-
-</html>
+ </html>
