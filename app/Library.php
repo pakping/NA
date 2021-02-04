6893d0f2aaf29d953d34b47871bebc3ffb9260f9
jee <guitar_104@hotmail.com>
2021-02-04 12:52:33 +0700

    card

--- app/Library.php
+++ app/Library.php
@@ -5,94 +5,59 @@ include '../auth/Sessionpersist.php';
 <html lang="en">
 
 <head>
-<!-- Required meta tags
+    <!-- Required meta tags
 ===================================================================================================-->
     <title>Hello, world!</title>
     <meta charset="utf-8">
     <META NAME="robots" CONTENT="noindex,onfollow">
- <!-- END Required meta tags 
-===================================================================================================-->   
+    <!-- END Required meta tags 
+===================================================================================================-->
 
 
-<?php
+    <?php
     include '../components/head/head.php'
     ?>
-<style>
-.card {
-  background: #fff;
-  border-radius: 2px;
-  display: inline-block;
-  height: 300px;
-  margin: 1rem;
-  position: relative;
-  width: 300px;
-}
+    <style>
+        .card-1 {
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+            transition: all 0.3s cubic-bezier(.25, .8, .25, 1);
+        }
 
-.card-1 {
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-}
+        .card-1:hover {
+            box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+        }
 
-.card-1:hover {
-  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
+    </style>
 
-/* .card-2 {
-  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-}
-
-.card-3 {
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-}
-
-.card-4 {
-  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-
-.card-5 {
-  box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
-} */
-
-</style>
-    
 </head>
 
 <body>
 
-<!-- Navigation
+    <!-- Navigation
 =================================================================================================== -->
     <?php
     include('../components/navbar/navbar-2.php');
     ?>
 
- <!-- END Navigation
-=================================================================================================== -->   
-<!-- <div class="card"></div> -->
-<div class="card card-1"></div>
-<!-- <div class="card card-2"></div> -->
-<!-- <div class="card card-3"></div>
-<div class="card card-4"></div>
-<div class="card card-5"></div> -->
-
-<br><br><br>
+    <!-- END Navigation
+=================================================================================================== -->
+    <br>
 
 
-<!-- *** Page Content 
+    <!-- *** Page Content 
 =================================================================================================-->
     <div class="container">
 
-<!-- Page Heading 
+        <!-- Page Heading 
 =================================================================================================-->
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
-                
-                <li class="breadcrumb-item "><a href="../Library.php">Library</a></li>
 
-                <li class="breadcrumb-item active" aria-current="page"> item</li>
+                <li class="breadcrumb-item active" aria-current="page"> Library</li>
             </ol>
         </nav>
-<!--END Page Heading 
+        <!--END Page Heading 
 =================================================================================================-->
 
         <h1 class="my-4">Page Heading
@@ -100,47 +65,49 @@ include '../auth/Sessionpersist.php';
         </h1>
         <div class="row">
 
- <!-- while แสดงรูปภาพ
+            <!-- while แสดงรูปภาพ
 ================================================================================================= -->
             <?php
-             require "../db/connect.php";
-             $Squery = "SELECT * FROM tagmaster ORDER BY Tag DESC";
-             if ($result = mysqli_query($con, $Squery)) {
-                 while ($img = mysqli_fetch_array($result)) {
+            require "../db/connect.php";
+            $Squery = "SELECT * FROM tagmaster ORDER BY Tag DESC";
+            if ($result = mysqli_query($con, $Squery)) {
+                while ($img = mysqli_fetch_array($result)) {
 
             ?>
-<!-- END while แสดงรูปภาพ
+                    <!-- END while แสดงรูปภาพ
 ================================================================================================= -->
 
 
-<!-- card แสดงรูปภาพ
+                    <!-- card แสดงรูปภาพ
  =================================================================================================-->
- 
-                                <div class="col-xl-4 col-md-6 mb-4">
-									<div class="card border-0 shadow">
-                                    <img src="<?php echo $img['path'];?>" class="card-img-top" alt="...">
-										<div class="card-body text-center">
-											<h5 class="card-title">สินค้าประเภท <?php echo $img['Tag']; ?></h5>
-											<form action= 'item.php' method= "POST">
-											<input type='hidden' name='directory' value=" <?php echo $img["Tag"] ?>"/>
-                                            
-                                            <button type="submit" class="btn btn-outline-primary btn-auto btn-block">More</button>
-											</form>
-										</div>
-									</div>
-								</div>
-                <!-- /.row -->
+
+                    <div class="col-xl-4 col-md-6 mb-4">
+                        <div class="card card card-1">
+                            <div style="height: 200px; overflow: hidden;">
+                                <img src="<?php echo $img['path']; ?>" class="card-img-top" alt="" style="width: 100%;">
+                            </div>
+                            <div class="card-body text-center">
+                                <h5 class="card-title">สินค้าประเภท <?php echo $img['Tag']; ?></h5>
+                                <form action='item.php' method="POST">
+                                    <input type='hidden' name='directory' value=" <?php echo $img["Tag"] ?>" />
+
+                                    <button type="submit" class="btn btn-outline-primary btn-auto btn-block">More</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
             <?php
+                }
             }
-            }
-        
+
             ?>
         </div>
 
-<!-- card แสดงรูปภาพ
+        <!-- card แสดงรูปภาพ
  =================================================================================================-->
 
-<!-- Pagination  
+        <!-- Pagination  
 =================================================================================================-->
         <ul class="pagination justify-content-center">
             <li class="page-item">
@@ -165,21 +132,21 @@ include '../auth/Sessionpersist.php';
                 </a>
             </li>
         </ul>
-<!--END Pagination  
+        <!--END Pagination  
 =================================================================================================-->
 
     </div>
     <!-- /.container -->
 
- <!--***END  Page Content 
+    <!--***END  Page Content 
 =================================================================================================-->
 
- <!-- Footer 
+    <!-- Footer 
 =================================================================================================-->
     <?php
     include('../components/footer.php')
     ?>
-<!--END Footer 
+    <!--END Footer 
 =================================================================================================-->
 </body>
 
