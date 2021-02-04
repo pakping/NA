94ff018ee31a965ac548d9d481d2f3ed8c33dd8c
pakping <pakping-up@hotmai.com>
2021-02-04 08:48:03 +0700

    55

--- components/header.php
+++ components/header.php
@@ -71,7 +71,7 @@
 
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <svg class="bd-placeholder-img" width="100%" height="100%"style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
         <rect width="100%" height="100%" fill="#777" />
       </svg>
       <div class="container">
