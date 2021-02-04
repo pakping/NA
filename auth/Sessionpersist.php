f1e8af28b50c8e5a329151fa63d9993650ed996e
MainCom <up.localhp@gmail.com>
2021-02-04 14:04:36 +0700

    path fixed

--- auth/Sessionpersist.php
+++ auth/Sessionpersist.php
@@ -14,7 +14,7 @@ if (isset($_SESSION)) {
         } 
         elseif ($_SESSION['type'] == 'guest') {
             if ($content != 'everyone'){
-            header("location:../index.php");
+            header("location:./index.php");
             }
         }
     }
