5d50b497a14ce5a6d71471f135d606a27c5cb8b1
MainCom <up.localhp@gmail.com>
2021-02-09 15:44:32 +0700

    sad

--- admin/edit-user.php
+++ admin/edit-user.php
@@ -29,7 +29,6 @@ require "../auth/Sessionpersist.php"
                         <th scope="col">ลำดับที่</th>
                         <th scope="col">Username</th>
                         <th scope="col">Password</th>
-                        <th scope="col">Type</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
@@ -45,65 +44,15 @@ require "../auth/Sessionpersist.php"
                             <th scope="row"><?php echo $a ?></th>
                             <td><?php echo $row['Username']; ?></td>
                             <td><?php echo $row['Password']; ?></td>
-                            <td><?php echo $row['Access']; ?></td>
                             <td>
                                 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                                    <!-- ปุ่มแก้ไข -->
-                                    <form action="" method="post">
-                                        <!-- Button trigger modal -->
-                                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editcaseeditcase<?php $a ?>">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </button>
-                                        <!-- Modal -->
-                                        <div class="modal fade" id="editcaseeditcase<?php $a ?>" tabindex="-1" aria-labelledby="editcaseeditcase<?php $a ?>Label" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="editcaseeditcase<?php $a ?>Label">แก้ไขรายละเอียดคุณ <?php echo $a . $row['Username']; ?></h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <p>Username</p>
-                                                        <input type="text" class="form-control" id="floatingInputValue" placeholder="" value="<?php echo $row['Username']; ?>">
-                                                        <br>
-                                                        <p>Password</p>
-                                                        <input type="text" class="form-control" id="floatingInputValue" placeholder="" value="<?php echo $row['Password']; ?>">
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                        <button type="button" class="btn btn-primary">Save changes</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-
                                     <!-- ปุ่มลบ -->
-                                    <!-- Button trigger modal -->
-                                    <form action="" method="post">
-                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                            <ion-icon name="trash-outline"></ion-icon>
-                                        </button>
-
-                                        <!-- Modal -->
-                                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel">ลบบัญชีผู้ใช้</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <p>โปรดยืนยันการลบ คุณ <?php echo $row['Username'] ?></p> 
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                        <button type="button" class="btn btn-danger">Delete</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                    <form action="../function/deluser.php" method="post">
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                    <input type="hidden" name='user' value="<?php echo $row['Username'];?>">
                                     </form>
+                                                    
+                                    
                                 </div>
                             </td>
                         </tr>
