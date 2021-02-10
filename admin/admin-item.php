3b57fa6912a78b30872ebb1c9423599c3ea83019
pakping <pakping-up@hotmai.com>
2021-02-10 09:34:19 +0700

    icon btn  item.php

--- admin/admin-item.php
+++ admin/admin-item.php
@@ -102,9 +102,11 @@
              </ol>
 
              <form action="" method='post'>
-                 <button type='btn' class="btn btn-primary">Back</button>
+                 <button type='btn' class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                         <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
+                     </svg>Back</button>
                  <input type='hidden' name='directory2' value='<?php end($_SESSION['page']) ?>'>
-                 
+
              </form>
              <!-- <li class="breadcrumb-item active" aria-current="page"> Library</li> -->
 
